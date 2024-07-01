07fe581681fa7ed2ea78d6ef9ace85e80b6ac96e
shukumar-542 <shukumar542@gmail.com>
2024-07-01 11:54:58 +0600

    Implement update user information

--- index.js
+++ index.js
@@ -103,6 +103,7 @@ async function run() {
 
         app.get('/api/v1/userInfo/:email', async (req, res) => {
             const email = req.params.email;
+
             const result = await userCollection.findOne({ email: email })
             res.json(result)
 
